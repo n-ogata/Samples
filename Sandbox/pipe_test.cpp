2913bb1631fc170f4dc9c4ac7aa8987b22feb4e2
Natsuki Ogata <n.ogata@topcon.co.jp>
2019-11-19 15:31:44 +0900

    確認内容を強化

--- Sandbox/pipe_test.cpp
+++ Sandbox/pipe_test.cpp
@@ -6,6 +6,7 @@
 #include <initializer_list>
 #include <iostream>
 #include <iterator>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -40,53 +41,78 @@ bool fork_execve_stdio(const string& path, const initializer_list<string>& args,
                        const initializer_list<string>& envs,
                        const string& stdin_str, string& stdout_str,
                        int& exit_status) {
-    array<int, 2> pipe_p_to_c{}; // 親から子への通信用パイプ
-    array<int, 2> pipe_c_to_p{}; // 子から親への通信用パイプ
-    if (::pipe(pipe_p_to_c.data()) != 0) {
+    enum
+    {
+        R,  // Read
+        W,  // Write
+        PipeCount,
+    };
+
+    const auto pipe_deleter = [](int* pipe_pointer) { ::close(*pipe_pointer); };
+    using PipeCloser        = unique_ptr<int, decltype(pipe_deleter)>;
+
+    array<int, PipeCount> pipe_p2c{};  // 親から子への通信用パイプ
+    array<int, PipeCount> pipe_c2p{};  // 子から親への通信用パイプ
+    if (::pipe(pipe_p2c.data()) != 0) {
         cerr << "can't create pipe(in)" << endl;
         return false;
     }
-    if (::pipe(pipe_c_to_p.data()) != 0) {
+    array<PipeCloser, PipeCount> pc_p2c{
+        PipeCloser(&pipe_p2c[R], pipe_deleter),
+        PipeCloser(&pipe_p2c[W], pipe_deleter),
+    };
+
+    if (::pipe(pipe_c2p.data()) != 0) {
         cerr << "can't create pipe(out)" << endl;
         return false;
     }
+    array<PipeCloser, PipeCount> pc_c2p{
+        PipeCloser(&pipe_c2p[R], pipe_deleter),
+        PipeCloser(&pipe_c2p[W], pipe_deleter),
+    };
 
     const int ret_fork = ::fork();
     if (ret_fork < 0) {
+        cerr << "can't fork" << endl;
         return false;
     }
     if (ret_fork == 0) {
+        // 子プロセス側
+
         // 標準入力をパイプの出力に差し替える
-        if (::dup2(pipe_p_to_c[0], STDIN_FILENO) != STDIN_FILENO) {
+        if (::dup2(pipe_p2c[R], STDIN_FILENO) != STDIN_FILENO) {
             cerr << "can't replace pipe(in)" << endl;
             _exit(-1);
         }
 
-        // パイプの入力にエコーバックを詰める
-        const string stdin_line(stdin_str + '\n');  // 改行無いと終端が判別できない
-        if (::write(pipe_p_to_c[1], stdin_line.c_str(), stdin_line.length()) < 0) {
-            cerr << "can't echo back pipe." << endl;
-            _exit(-1);
+        if (!stdin_str.empty()) {
+            // パイプの入力にエコーバックを詰める
+            const string stdin_line(stdin_str + '\n');  // 改行無いと終端が判別できない
+            if (::write(pipe_p2c[W], stdin_line.c_str(), stdin_line.length()) < 0) {
+                cerr << "can't echo back pipe." << endl;
+                _exit(-1);
+            }
         }
 
         // 標準出力をOutput用のパイプの入力に差し替える
-        if (::dup2(pipe_c_to_p[1], STDOUT_FILENO) != STDOUT_FILENO) {
+        if (::dup2(pipe_c2p[W], STDOUT_FILENO) != STDOUT_FILENO) {
             cerr << "can't replace pipe(out)" << endl;
             _exit(-1);
         }
 
-        for (const int pipe : pipe_p_to_c) ::close(pipe);
-        for (const int pipe : pipe_c_to_p) ::close(pipe);
+        for (auto& closer : pc_p2c) closer.reset();
+        for (auto& closer : pc_c2p) closer.reset();
 
-        // 子プロセスで実行する
+        // 実行
         exit_status = execve(path, args, envs);
         _exit(exit_status);
         return false;
     }
 
-    // パイプのディスクリプタを保存する
-    ::close(pipe_p_to_c[0]);
-    ::close(pipe_c_to_p[1]);
+    pc_p2c[R].reset();
+    pc_c2p[W].reset();
+    pc_p2c[W].release();
+    pc_c2p[R].release();
 
     int         status      = 0;
     const pid_t ret_waitpid = ::waitpid(ret_fork, &status, 0);
@@ -97,21 +123,36 @@ bool fork_execve_stdio(const string& path, const initializer_list<string>& args,
         return false;
     }
 
-    array<char, 2> buffer;
+    exit_status = WEXITSTATUS(status);
+
+    stdout_str.clear();
+    array<char, 0xff> buffer;
     while (true) {
-        const int ret_read = ::read(pipe_c_to_p[0], buffer.data(), buffer.size());
-        if (ret_read == 0) break;
-        if (ret_read < 0) return false;
+        const int ret_read = ::read(pipe_c2p[R], buffer.data(), buffer.size());
+        if (ret_read == 0) break;  // EOD
+        if (ret_read < 0) {
+            cerr << "can't read pipe(out)" << endl;
+            return false;
+        }
         stdout_str += string(buffer.data(), ret_read);
     }
 
-    exit_status = WEXITSTATUS(status);
     return true;
 }
 
 int main() {
     string stdout_str;
     int exit_status = 0;
-    fork_execve_stdio("./echo_cpp", {}, {}, "Hoge", stdout_str, exit_status);
-    cout << stdout_str << endl;
+    if (fork_execve_stdio("./echo_cpp", {}, {}, "Hoge", stdout_str, exit_status)) {
+        cout << stdout_str << exit_status << endl;
+    }
+    if (fork_execve_stdio("./helloworld_cpp", {}, {}, "", stdout_str, exit_status)) {
+        cout << stdout_str << exit_status << endl;
+    }
+    if (fork_execve_stdio("/bin/ls", {}, {}, "", stdout_str, exit_status)) {
+        cout << stdout_str << exit_status << endl;
+    }
+    if (fork_execve_stdio("./not_exe", {}, {}, "", stdout_str, exit_status)) {
+        cout << stdout_str << exit_status << endl;
+    }
 }
